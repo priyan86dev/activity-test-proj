b875138746980c83d199713ac1e789b34a071a30
priyan86dev <priyan86dev@gmail.com>
2021-05-07 08:36:56 +0530

    initial commit

---
+++ src/main/java/com/testactivity/ActivityTestProjApplication.java
@@ -0,0 +1,13 @@
+package com.testactivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityTestProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivityTestProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testactivity/ActivityTestProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testactivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityTestProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
